a9e43f80575c1c4e0f17ffe231f417bbae8f1d37
Gabriel Ferreira <gabriellfsg00@gmail.com>
2023-05-17 19:04:23 -0300

    Saving changes

--- app/src/main/java/com/example/goalapp/GoalActivity.kt
+++ app/src/main/java/com/example/goalapp/GoalActivity.kt
@@ -1,11 +1,22 @@
 package com.example.goalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.goalapp.databinding.ActivityGoalBinding
 
 class GoalActivity : AppCompatActivity() {
+    private var binding: ActivityGoalBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_goal)
+        binding = ActivityGoalBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.newGoalBtn?.setOnClickListener {
+            val intent = Intent(this, AddGoalActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
